def085b6464cd90c2f5da287b96cc798d0199f80
saitejakesa <kesasaiteja1996@gmail.com>
2023-01-08 18:26:32 +0530

    Afterverify3

--- routes/users.js
+++ routes/users.js
@@ -85,7 +85,7 @@ router.post("/forgetpassword",async(req,res)=>{
         {email:oldUser.email,id:oldUser.id},
         secret,
         {expiresIn:'5m'})
-      const link=`http://localhost:3000/users/validate/${oldUser.id}/${token}`;
+      const link=`https://flourishing-fox-b9b3da.netlify.app/${oldUser.id}/${token}`;
       var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
