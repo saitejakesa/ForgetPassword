002d23c33bddbb9f48f1206b1e6d890b233f59d2
saitejakesa <52096261+saitejakesa@users.noreply.github.com>
2023-01-21 16:57:14 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -85,7 +85,7 @@ router.post("/forgetpassword",async(req,res)=>{
         {email:oldUser.email,id:oldUser.id},
         secret,
         {expiresIn:'5m'})
-      const link=`http://localhost:3000/reset/${oldUser.id}/${token}`;
+      const link=`https://forget-password-fc45sukh4-saitejakesa.vercel.app/reset/${oldUser.id}/${token}`;
       var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
